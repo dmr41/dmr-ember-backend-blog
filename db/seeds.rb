7aedf637204ffcef4d609cdf43b62e5b8e4290dc
David Rivers <dmr41.rivers@gmail.com>
2015-02-12 12:59:41 -0700

    added more seed data in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@ Post.delete_all
 user1 = User.create!( name: Faker::Name.name )
 user2 = User.create!( name: Faker::Name.name )
 user3 = User.create!( name: Faker::Name.name )
+user4 = User.create!( name: Faker::Name.name )
+user5 = User.create!( name: Faker::Name.name )
 
 post1 = Post.create!(
   title: Faker::Lorem.sentence,
@@ -24,9 +26,43 @@ post3 = Post.create!(
   user: user2
 )
 
+post4 = Post.create!(
+  title: Faker::Lorem.sentence,
+  body: Faker::Lorem.paragraphs.join("\n\n"),
+  user: user2
+)
+post5 = Post.create!(
+  title: Faker::Lorem.sentence,
+  body: Faker::Lorem.paragraphs.join("\n\n"),
+  user: user4
+)
+post5 = Post.create!(
+  title: Faker::Lorem.sentence,
+  body: Faker::Lorem.paragraphs.join("\n\n"),
+  user: user5
+)
+
 Comment.create!(
   user: user3,
   post: post1,
+  opinion: Faker::Lorem.paragraph
+)
+
+Comment.create!(
+  user: user4,
+  post: post1,
+  opinion: Faker::Lorem.paragraph
+)
+
+Comment.create!(
+  user: user4,
+  post: post2,
+  opinion: Faker::Lorem.paragraph
+)
+
+Comment.create!(
+  user: user4,
+  post: post3,
   opinion: Faker::Lorem.paragraph
 )
 
@@ -36,6 +72,17 @@ Comment.create!(
   opinion: Faker::Lorem.paragraph
 )
 
+Comment.create!(
+  user: user3,
+  post: post1,
+  opinion: Faker::Lorem.paragraph
+)
+
+Comment.create!(
+  user: user1,
+  post: post3,
+  opinion: Faker::Lorem.paragraph
+)
 Comment.create!(
   user: user1,
   post: post3,
